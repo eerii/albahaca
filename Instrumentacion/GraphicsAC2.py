f7f72ce369aa50da7623b77fc1724692d9a556e9
josekoalas <jose.pazos.perez@rai.usc.es>
2020-03-25 17:52:34 +0100

    Pre-Revisión

--- Instrumentacion/GraphicsAC2.py
+++ Instrumentacion/GraphicsAC2.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.optimize import curve_fit
 
 #Configure matplotlib
 matplotlib.use("pgf")
@@ -12,29 +13,67 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
-name = "CA4"
+name = "CA5"
 
 #Read from CSV to pandas dataframe
 d = pd.read_csv(name + ".csv", sep=';', decimal=',')
+d2 = pd.read_csv(name + "All.csv", sep=';', decimal=',')
 
-x = d["f"]
-vmr = d["VmR"]
-vmc = d["VmC"]
-y = vmr / vmc
+x = d["logf"]
+y = d["phideg"]
+x2 = d2["logf"]
+y2 = d2["phideg"]
+#vmr = d["VmR"]
+#vmc = d["VmC"]
+#y = vmr / vmc
 
 #Scatter Plot
 fig = plt.figure()
 fig.set_size_inches(w=5, h=3.5)
 
 plt.scatter(x,y, zorder=2)
+plt.scatter(x2,y2, zorder=1, color="dodgerblue")
 
-m, b = np.polyfit(x, y, 1)
-plt.plot(x, m*x + b, zorder=1, color="skyblue")
-x2 = (1 - b)/m
-plt.scatter(x2, 1, zorder=3, color="tomato")
-print("Frecuencia corte:", x2)
+#Ajuste a recta
+#m, b = np.polyfit(x2, y2, 1)
+#plt.plot(x, m*x + b, zorder=1, color="skyblue")
+#x3 = (-45 - b)/m
+#plt.scatter(x3, -45, zorder=3, color="tomato")
 
-plt.ylabel('VmR/VmC', rotation=0, labelpad=25)
-plt.xlabel('f (Hz)')
+
+#Ajuste a polinomio
+#p2 = np.polynomial.Polynomial.fit(x2, y2, 2)
+#p3 = np.polynomial.Polynomial.fit(x2, y2, 3)
+#p4 = np.polynomial.Polynomial.fit(x2, y2, 4)
+#p5 = np.polynomial.Polynomial.fit(x2, y2, 5)
+#plt.plot(*p2.linspace(), color="tomato")
+#plt.plot(*p3.linspace(), color="gold")
+#plt.plot(*p4.linspace(), color="limegreen")
+#plt.plot(*p5.linspace(), color="skyblue")
+
+#Ajuste a sigmoide
+def fsigmoid(x, a, b):
+    return 1.0 / (1.0 + np.exp(-a*(x-b)))
+
+y3 = (y2 + 100) * 0.01
+x3 = x2 - 3.1226
+
+popt, pcov = curve_fit(fsigmoid, x3, y3, method='dogbox')
+
+xs = np.arange(-3., +3., 0.01)
+ys = (fsigmoid(xs, *popt) * 100) - 100
+xs += 3.1226
+plt.plot(xs, ys, color="gold")
+
+for i in range(len(ys)):
+    if ys[i] < -44.5 and ys[i] > -45.5:
+        plt.scatter(xs[i], ys[i], color="tomato", zorder=4)
+        print("Frecuencia de corte:", xs[i], 10**xs[i])
+
+
+#print("Frecuencia corte:", x3, 10**x3)
+
+plt.ylabel('$\\phi$ \\textit{(º)}', rotation=0, labelpad=25)
+plt.xlabel('log f')
 #plt.title('Voltaje (V) frente a intensidad (I)')
-plt.savefig(name + "V.pgf", bbox_inches = "tight")
+plt.savefig(name + "All.pgf", bbox_inches = "tight")
--- Instrumentacion/GraphicsAC3.py
+++ Instrumentacion/GraphicsAC3.py
@@ -14,19 +14,25 @@ matplotlib.rcParams.update({
 
 fig, ax = plt.subplots()
 fig.set_size_inches(w=5, h=2.5)
-fig.patch.set_visible(False)
-ax.axis('off')
+#fig.patch.set_visible(False)
+#ax.axis('off')
 
 #Curves
-x = np.arange(0, 2 * np.pi, 0.1);
+#x = np.arange(0, 2 * np.pi, 0.1);
+x = np.arange(4.6, 4.8, 0.0005);
 yg = 0.65 * np.cos(x)
-yr = 0.65 * np.cos(x + (1/6) * np.pi)
+#yr = 0.65 * np.cos(x + (1/6) * np.pi)
+x2 = x + 0.0276
+yr = 0.65 * np.cos(x2)
 
-plt.plot(x,yg, color="dodgerblue", zorder=2)
-plt.plot(x,yr, color="tomato", zorder=2)
+noise1 = np.random.normal(0, 0.01, yg.shape)
+noise2 = np.random.normal(0, 0.01, yr.shape)
 
-plt.text(np.pi * 0.5 - 0.2, 0.45, "$V_G$", fontsize=12, color="#1f77b4")
-plt.text(np.pi * 0.5 - 0.5, -0.65, "$V_R$", fontsize=12, color="tomato")
+plt.plot(x,yg+noise1, color="dodgerblue", zorder=2)
+plt.plot(x,yr+noise2, color="tomato", zorder=2)
+
+#plt.text(np.pi * 0.5 - 0.2, 0.45, "$V_G$", fontsize=12, color="#1f77b4")
+#plt.text(np.pi * 0.5 - 0.5, -0.65, "$V_R$", fontsize=12, color="tomato")
 
 #Intersecions
 ig = (3/2) * np.pi
@@ -34,30 +40,25 @@ ir = (3/2) * np.pi - (1/6) * np.pi
 yi = yv = np.arange(-0.85, 0.85, 0.1)
 xi = np.ones(len(yi))
 
-plt.scatter(ig, 0, color="dodgerblue", zorder=4)
-plt.scatter(ir, 0, color="lightsalmon", zorder=4)
+#plt.scatter(ig, 0, color="dodgerblue", zorder=4)
+#plt.scatter(ir, 0, color="lightsalmon", zorder=4)
 
-plt.plot(xi * ig, yi, color="skyblue", zorder=3)
-plt.plot(xi * ir, yi, color="peachpuff", zorder=3)
+#plt.plot(xi * ig, yi, color="skyblue", zorder=3)
+#plt.plot(xi * ir, yi, color="peachpuff", zorder=3)
 
-plt.text(ig + 0.2, -0.8, "t1", fontsize=12, color="#1f77b4")
-plt.text(ir - 0.5, -0.8, "t2", fontsize=12, color="tomato")
+#plt.text(ig + 0.2, -0.8, "t1", fontsize=12, color="#1f77b4")
+#plt.text(ir - 0.5, -0.8, "t2", fontsize=12, color="tomato")
 
 #Vertical Axis
 yv = np.arange(-1, 1, 0.1)
-xv = np.zeros(len(yv))
-plt.plot(xv, yv, color="black", zorder=0, linewidth=0.3)
+xv = np.ones(len(yv))
+#plt.plot(xv, yv, color="black", zorder=0, linewidth=0.3)
 #Horizontal Axis
-xh = np.arange(0, 2 * np.pi, 0.2)
+#xh = np.arange(0, 2 * np.pi, 0.2)
+xh = np.arange(4.6, 4.85, 0.05)
 yh = np.zeros(len(xh))
 plt.plot(xh, yh, color="black", zorder=0, linewidth=0.3)
-plt.text(1.95 * np.pi, -0.2, "t", fontsize=12, color="black")
-
-#m, b = np.polyfit(x, y, 1)
-#plt.plot(x, m*x + b, zorder=1, color="skyblue")
-#x2 = (1 - b)/m
-#plt.scatter(x2, 1, zorder=3, color="tomato")
-#print("Frecuencia corte:", x2)
+#plt.text(1.95 * np.pi, -0.2, "t", fontsize=12, color="black")
 
 #plt.title('Voltaje (V) frente a intensidad (I)')
-plt.savefig("Osciloscope1.pgf", bbox_inches = "tight")
+plt.savefig("Osciloscope3.pgf", bbox_inches = "tight")
