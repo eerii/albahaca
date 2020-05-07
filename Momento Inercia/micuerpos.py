2da4432cf64f64d2cc1de1d32b623de8af2d3036
josekoalas <jose.pazos.perez@rai.usc.es>
2020-05-07 16:53:20 +0200

    Momento Inercia Cuerpos

--- Momento Inercia/micuerpos.py
+++ Momento Inercia/micuerpos.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import uncertainties as unc
 
 import sys
 sys.path.insert(1, '../Base')
@@ -8,7 +9,64 @@ from varias_medidas import tratamiento_datos as tdatos
 import mpl_config as mpl
 
 #Datos
+sb = 0.001
+D = 0.02167 #IMPORTANTE: CAMBIAR!!!!
+sD = 0.00048
+
 d = pd.read_csv("MI2_Cuerpos.csv", sep=';', decimal=',')
 td = d["TDisco"].to_numpy()
-sb = 0.001
-x, m, sc, e = tdatos(td, sb)
+xd, md, scd, ed = tdatos(td, sb)
+tc = d["TCil"].to_numpy()
+xc, mc, scc, ec = tdatos(tc, sb)
+
+#DISCO
+print("---\nDisco\n---")
+
+#Momento Inercia Experimental
+Td = 2*md
+sTd = 2*scd
+Id = (Td**2 * D) / (4*np.pi**2)
+
+#Propagación Incertidumbres
+sId = ((((Td*D) / (2*np.pi**2))**2 * sTd**2) + ((Td**2 / (4*np.pi**2))**2 * sD**2))**0.5
+#Tdu = unc.ufloat(Td, sTd); Du = unc.ufloat(D, sD); Iu = (Tdu**2 * Du) / (4*np.pi**2)
+
+#Momento Inercia Teórico
+Md = 0.2959
+sMd = 0.00001
+Rd = 0.108
+sRd = 0.001
+Idt = (Md * Rd**2) / 2
+
+#Propagación Incertidumbres
+sIdt = (((Rd**2 / 2)**2 * sMd**2) + ((Md * Rd)**2 * sRd**2))**0.5
+#Mdu = unc.ufloat(Md, sMd); Rdu = unc.ufloat(Rd, sRd); Idu = (Mdu * Rdu**2) / 2
+
+print("I(exp) = {} +/- {}".format(Id, sId))
+print("I(teo) = {} +/- {}".format(Idt, sIdt))
+
+
+#CILINDRO
+print("---\nCilindro\n---")
+
+#Momento Inercia Experimental
+Tc = 2*mc
+sTc = 2*scc
+Ic = (Tc**2 * D) / (4*np.pi**2)
+
+#Propagación Incertidumbres
+sIc = ((((Tc*D) / (2*np.pi**2))**2 * sTc**2) + ((Tc**2 / (4*np.pi**2))**2 * sD**2))**0.5
+
+#Momento Inercia Teórico
+Mc = 0.3731
+sMc = 0.00001
+Rc = 0.05
+sRc = 0.001
+Ict = (Mc * Rc**2) / 2
+
+#Propagación Incertidumbres
+sIct = (((Rc**2 / 2)**2 * sMc**2) + ((Mc * Rc)**2 * sRc**2))**0.5
+#Mdu = unc.ufloat(Md, sMd); Rdu = unc.ufloat(Rd, sRd); Idu = (Mdu * Rdu**2) / 2
+
+print("I(exp) = {} +/- {}".format(Ic, sIc))
+print("I(teo) = {} +/- {}".format(Ict, sIct))
