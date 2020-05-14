64953de378f2a2c88a18e2b86546c855c40cf24e
josekoalas <jose.pazos.perez@rai.usc.es>
2020-05-14 13:47:49 +0200

    Finalizar Electricidad (Faltan las conclusiones)

--- Electricidad/cc.py
+++ Electricidad/cc.py
@@ -7,7 +7,7 @@ sys.path.insert(1, '../Base')
 from reg_lin import reg_lin_w as rl
 import mpl_config as mpl
 
-mpl.inicio(4, [7., 5.])
+mpl.inicio(4)#, [7., 5.])
 
 D = [pd.read_csv("CC-{}.csv".format(i), sep=';', decimal=',') for i in range(1,4)]
 
@@ -17,13 +17,26 @@ LnI = np.asarray([[D[j]["LnI{}".format(i)].to_numpy() for i in range(1,5)] for j
 sLnI = np.asarray([[D[j]["sLnI{}".format(i)].to_numpy() for i in range(1,5)] for j in range(0,3)])
 
 #Selector de experimento
-exp = 0 #Número de experiencia (0-2)
+exp = 2 #Número de experiencia (0-2)
 rep = 3 #Número de repetición (0-3)
 
-fig, axs = plt.subplots(2, 2)
 c = ["royalblue", "mediumseagreen", "sandybrown", "tomato"]
 
+for r in range(4):
+    x = T[exp,r][~np.isnan(T[exp,r])]
+    y = LnI[exp,r][~np.isnan(LnI[exp,r])]
+    sy = sLnI[exp,r][~np.isnan(sLnI[exp,r])]
+
+    a, b = rl(x,y,sy)[0:2]
+
+    xr = np.linspace(min(x), max(x), 20)
+    yr = a + b*xr
+
+    plt.scatter(x, y, linewidth=0.5, label='$C_{}$'.format(r+1))
+    plt.plot(xr, yr)
+
 #Regresión lineal ponderada y gráficas
+"""fig, axs = plt.subplots(2, 2)
 for r in range(4):
     x = T[exp,r][~np.isnan(T[exp,r])]
     y = LnI[exp,r][~np.isnan(LnI[exp,r])]
@@ -39,18 +52,19 @@ for r in range(4):
 
     axs[xi, yi].scatter(x, y, color=c[r], edgecolors="black", linewidth=0.5)
     axs[xi, yi].plot(xr, yr, color=c[r])
-    axs[xi, yi].set_title('$R_{}$'.format(r+1))
+    axs[xi, yi].set_title('$C_{}$'.format(r+1))
 
 for ax in axs.flat:
     ax.set(xlabel='T(s)', ylabel='$\ln{I}(A)$')
-    ax.label_outer()
+    ax.label_outer()"""
 
-mpl.guardar("CC-1LR", "T(s)", "$\ln{I}(A)$", False, False)
+mpl.guardar("CC-3LR", "T(s)", "$\ln{I}(A)$")#, False, False)
 
 import uncertainties as unc
 from uncertainties.umath import *
-v = unc.ufloat(10, 0.1)
-c = unc.ufloat(3*10**(-5), 0.05*3*10**(-5))
-r = unc.ufloat(3.2*10**6, 0.05*3.2*10**6)
+v = unc.ufloat(10.2, 0.1)
+c = unc.ufloat(10**(-5), 0.05*10**(-5))
+r = unc.ufloat(4.4*10**6, 0.05*4.4*10**6)
 a = log(v/r)
 b = (-1)/(r*c)
+print("a = {:.2u}, b = {:.2u}".format(a, b))
